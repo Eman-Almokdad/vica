b8af99aee25c4e5bdd9fe4398d9d440afe332a7c
Eman-Almokdad <emanmokdad.101@gmail.com>
2024-01-05 19:51:55 +0200

    update

--- assets/script.js
+++ assets/script.js
@@ -15,14 +15,12 @@ window.addEventListener('scroll',  bg */ /* ()=>{
         nav.classList.remove("bgcolor")
     }
 } *///);
-let scroll=document.body.scrollTop;
+/* let scroll=document.body.scrollTop;
 let nav=document.querySelector("nav")
 window.addEventListener('scroll',()=>{
-    //scroll > 0 ? nav.classList.add("bgcolor") : nav.classList.remove("bgcolor");
+    
     console.log(scroll);
-    /* if(scroll > 0){
-        nav.classList.add("bgcolor")
-    } */
+
     if (scroll > 10) {
         nav.classList.remove("bgcolor")
     } else {
@@ -31,6 +29,27 @@ window.addEventListener('scroll',()=>{
     
     
 
-})
+}) */
 
+/* window.addEventListener('scroll', ()=> {
+     navbar = document.getElementsByClassName('navbar');
+    if (window.scrollY > 100) {
+    navbar.style.backgroundColor = '#333';
+    navbar.style.padding = '10px 0';
+    } else {
+    navbar.style.backgroundColor = 'transparent';
+    navbar.style.padding = '20px 0';
+    }
+});
+ */
+let navbar = document.querySelector(".navbar");
+window.addEventListener('scroll', ()=> {
+    if (window.scrollY > 100) {
+      console.log(window.scrollY)
+      navbar.classList.add("bgcolor");
+    } else {
+      
+      navbar.classList.remove("bgcolor");
+    }
+  });
 
